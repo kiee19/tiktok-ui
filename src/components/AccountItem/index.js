1da43e2bef489e8d8e13edcffaa6eeb4120c9676
kieeran <giakietgx@gmail.com>
2026-01-14 12:20:04 +0700

    xay dung ui phan header #4

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -9,7 +9,7 @@ function AccountItem() {
   return (
     <div className={cx('wrapper')}>
       <img
-        src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/b72e8d9ad9e0ec8113f83c9a3e22a831~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=75decf2c&x-expires=1765771200&x-signature=y7wm2RwNWFxCfk0HtjkAXhdZXrA%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my2"
+        src="https://jbagy.me/wp-content/uploads/2025/03/anh-avatar-dep-vo-tri-3.jpg"
         alt="Hoa"
         className={cx('avatar')}
       />
